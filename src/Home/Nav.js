1afe8afe2d11c416f41742045ff8167d0c197939
drewhsu86 <bowserhsu@gmail.com>
2020-04-28 15:52:00 -0400

    castle turret css complete

--- src/Home/Nav.js
+++ src/Home/Nav.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import NavButton from './NavButton'
+import NavCastle from './NavCastle'
 
 export default function Nav() {
   return (
     <nav className="navBar">
+      <NavCastle />
+
       <Link path="/">
         <h1>Knights of Coding</h1>
       </Link>
---
+++ src/Home/NavCastle.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavCastle.css'
+
+export default function NavCastle() {
+  return (
+    <div className="castle">
+      <div className="castleWall">
+
+        <div className="castleTurret" />
+        <div className="castleTurret" />
+        <div className="castleTurret" />
+        <div className="castleTurret optional" />
+        <div className="castleTurret optional" />
+        <div className="castleTurret optional" />
+
+      </div>
+    </div>
+  )
+}
